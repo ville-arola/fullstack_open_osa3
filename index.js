806489d144b8acf67600ff10d53b5115d12ba05a
Ville <veill@luukku.com>
2018-12-08 18:20:12 +0200

    Tehtävä 3.1

---
+++ index.js
@@ -0,0 +1,35 @@
+let persons = [
+    {
+        name: 'Arto Hellas',
+        number: '040-123456',
+        id: 1,
+    },
+    {
+        name: 'Martti Tienari',
+        number: '040-123456',
+        id: 2,
+    },
+    {
+        name: 'Arto Järvinen',
+        number: '040-123456',
+        id: 3,
+    },
+    {
+        name: 'Lea Kutvonen',
+        number: '040-123456',
+        id: 4,
+    },
+];
+
+const express = require('express')
+
+const app = express();
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
